1d22edec20955c213030ce0bf0ad582ba9d06441
silasprd <silas.prado98@gmail.com>
2024-10-08 17:41:50 -0300

    continuing model training

---
+++ README.md
@@ -0,0 +1,12 @@
+# Modelo de Classificação - Machine Learning
+
+## Este repositório contém a implementação de algoritmos de machine learning focados na classificação de espécies de peixes com base em atributos físicos como comprimento e peso. 
+
+### Utilizando Redes Neurais Artificiais (RNA), juntamente com outros algoritmos de classificação, o projeto busca:
+
+<ul>
+    <li>Analisar o desempenho de diferentes modelos em termos de acurácia e precisão.</li>
+    <li>Comparar e ajustar hiperparâmetros dos modelos de machine learning para obter melhores resultados.</li>
+    <li>Implementar métricas de avaliação para medir o impacto das predições.</li>
+    <li>Demonstrar o uso de validação cruzada para garantir que os modelos generalizem bem para novos dados.</li>
+</ul>
---
+++ classification2.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+wbc = pd.read_csv('fish_data.csv', encoding='ISO-8859-1')
+
+# Mapping species to numbers
+species_mapping = {species: idx for idx, species in enumerate(wbc['species'].unique())}
+y = wbc['species'].map(species_mapping)
+
+X = wbc[['length', 'weight']]
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# Instanciates a neural network classifier type with maximum of 2000 epochs 
+model = MLPClassifier(hidden_layer_sizes=(100,100,100), activation='tanh', learning_rate='adaptive', random_state=1, max_iter=2000)
+
+# Train model
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Accuracy
+acc = accuracy_score(y_test, y_pred)
+print("Acurácia: {:.2f}".format(acc))
+
+print('Matriz de Confusão')
+cm = confusion_matrix(y_test, y_pred, labels=y_train.unique())
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=species_mapping.keys())
+disp.plot()
+
+plt.xticks(rotation=35, ha='right')
+plt.show()
